963678729ff04f92edc34b9c59e0aaf811467127
Bruno Rafael <rbrunog21@gmail.com>
2026-07-17 17:58:41 -0300

    fix(unit-test): inject required repository

--- src/Services/Restaurant/RestaurantServices.Tests/CreateRestaurantServiceTest.cs
+++ src/Services/Restaurant/RestaurantServices.Tests/CreateRestaurantServiceTest.cs
@@ -2,6 +2,7 @@
 using Restaurant.Application.Interfaces;
 using Restaurant.Application.UseCases;
 using Restaurant.Domain.DTOs.Requests;
+using Restaurant.Domain.Entities;
 using Restaurant.Domain.Repositories;
 using Shouldly;
 
@@ -11,13 +12,15 @@ namespace RestaurantServices.Tests
     {
         private readonly ICreateRestaurantUseCase _createRestaurantUseCase;
         private readonly Mock<IRestaurantRepository> _mockRepository;
+        private readonly Mock<IRestaurantTableRepository> _mockRestaurantTableRepository;
         private readonly Mock<IUnitOfWork> _mockUow;
 
         public CreateRestaurantServiceTests()
         {
             _mockRepository = new Mock<IRestaurantRepository>();
+            _mockRestaurantTableRepository = new Mock<IRestaurantTableRepository>();
             _mockUow = new Mock<IUnitOfWork>();
-            _createRestaurantUseCase = new CreateRestaurantUseCase(_mockRepository.Object, _mockUow.Object);
+            _createRestaurantUseCase = new CreateRestaurantUseCase(_mockRepository.Object, _mockRestaurantTableRepository.Object, _mockUow.Object);
 
             _mockRepository
                 .Setup(x => x.AddAsync(It.IsAny<Restaurant.Domain.Entities.Restaurant>()))
@@ -26,13 +29,21 @@ namespace RestaurantServices.Tests
                     Id = Guid.NewGuid(),
                     Name = "Test Restaurant"
                 });
+            _mockRestaurantTableRepository
+                .Setup(x => x.AddAsync(It.IsAny<RestaurantTable>()))
+                .ReturnsAsync(new RestaurantTable
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Table 1",
+                    Seats = 2
+                });
             _mockUow.Setup(x => x.CommitAsync());
         }
 
         [Fact(DisplayName = "should be create a restaurant")]
         public async Task ShouldBeCreateRestaurant()
         {
-            var name = "Vitinho's Restaurant";
+            var name = "Restaurant";
 
             var response = await _createRestaurantUseCase.Execute(new CreateRestaurantRequest { Name = name });
 
